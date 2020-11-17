76bfe0b36cdbe67b3ff7ad00480549ce9db515a7
ni.burkin <evan.gelior@ya.ru>
2020-11-17 13:27:54 +0700

    (~) fix config voyager schema

--- config/voyager-schema.php
+++ config/voyager-schema.php
@@ -21,66 +21,57 @@ return [
     "menu" => [
         ["class" => \App\Schema\Tables\UserDataType::class],
         [
-            "custom" => [
-                'locale'     => 'voyager::seeders.menu_items.dashboard',
-                'route'      => 'voyager.dashboard',
-                'icon_class' => 'voyager-boat',
-            ]
+            "custom"     => true,
+            'locale'     => 'voyager::seeders.menu_items.dashboard',
+            'route'      => 'voyager.dashboard',
+            'icon_class' => 'voyager-boat',
         ],
         [
-            "custom" => [
-                'locale'     => 'voyager::seeders.menu_items.media',
-                'route'      => 'voyager.media.index',
-                'icon_class' => 'voyager-images',
-            ]
+            "custom"     => true,
+            'locale'     => 'voyager::seeders.menu_items.media',
+            'route'      => 'voyager.media.index',
+            'icon_class' => 'voyager-images',
         ],
         [
-            "custom" => [
-                'locale'     => 'voyager::seeders.menu_items.roles',
-                'route'      => 'voyager.roles.index',
-                'icon_class' => 'voyager-lock',
-            ]
+            "custom"     => true,
+            'locale'     => 'voyager::seeders.menu_items.roles',
+            'route'      => 'voyager.roles.index',
+            'icon_class' => 'voyager-lock',
         ],
         [
-            "custom" => [
-                'locale'     => 'voyager::seeders.menu_items.tools',
-                'icon_class' => 'voyager-tools',
-            ],
-            "children" => [
+            "custom"     => true,
+            'locale'     => 'voyager::seeders.menu_items.tools',
+            'icon_class' => 'voyager-tools',
+            "children"   => [
                 [
-                    "custom" => [
-                        'locale'     => 'voyager::seeders.menu_items.menu_builder',
-                        'route'      => 'voyager.menus.index',
-                        'icon_class' => 'voyager-list',
-                    ]
+                    "custom"     => true,
+                    'locale'     => 'voyager::seeders.menu_items.menu_builder',
+                    'route'      => 'voyager.menus.index',
+                    'icon_class' => 'voyager-list',
                 ],
                 [
-                    "custom" => [
-                        'locale'     => 'voyager::seeders.menu_items.database',
-                        'route'      => 'voyager.database.index',
-                        'icon_class' => 'voyager-data',
-                    ]
+                    "custom"     => true,
+                    'locale'     => 'voyager::seeders.menu_items.database',
+                    'route'      => 'voyager.database.index',
+                    'icon_class' => 'voyager-data',
                 ],
                 [
-                    "custom" => [
-                        'locale'     => 'voyager::seeders.menu_items.compass',
-                        'route'      => 'voyager.compass.index',
-                        'icon_class' => 'voyager-compass',
-                    ]
+                    "custom"     => true,
+                    'locale'     => 'voyager::seeders.menu_items.compass',
+                    'route'      => 'voyager.compass.index',
+                    'icon_class' => 'voyager-compass',
                 ],
                 [
-                    "custom" => [
-                        'locale'     => 'voyager::seeders.menu_items.bread',
-                        'route'      => 'voyager.bread.index',
-                        'icon_class' => 'voyager-bread',
-                    ]
+                    "custom"     => true,
+                    'locale'     => 'voyager::seeders.menu_items.bread',
+                    'route'      => 'voyager.bread.index',
+                    'icon_class' => 'voyager-bread',
                 ],
                 [
-                    "custom" => [
-                        'locale'     => 'voyager::seeders.menu_items.settings',
-                        'route'      => 'voyager.settings.index',
-                        'icon_class' => 'voyager-settings',
-                    ]
+                    "custom"     => true,
+                    'locale'     => 'voyager::seeders.menu_items.settings',
+                    'route'      => 'voyager.settings.index',
+                    'icon_class' => 'voyager-settings',
                 ],
             ]
         ],
