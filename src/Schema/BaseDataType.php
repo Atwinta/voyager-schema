4cbd92720c7b0a3487810da78ea4de065c9c97ff
Никита Буркин <evan.gelior@ya.ru>
2021-01-28 10:42:17 +0700

    (+) add option list getter in base data type

--- src/Schema/BaseDataType.php
+++ src/Schema/BaseDataType.php
@@ -26,4 +26,19 @@ abstract class BaseDataType implements DataTypeInterface
         $model = $this->model();
         return ($this->model ?? ($this->model = new $model));
     }
+
+    /**
+     * @param string $prefix
+     * @param \Illuminate\Support\Collection|array $keys
+     * @param string|null $locale
+     * @return array
+     */
+    protected function getOptionList(string $prefix, $keys, string $locale = null)
+    {
+        $list = [];
+        foreach ($keys as $key) {
+            $list[$key] = __("{$prefix}.{$key}", [], $locale);
+        }
+        return $list;
+    }
 }
